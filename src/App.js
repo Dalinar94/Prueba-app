4605f11897f70a4b093ead936c3566bd7431fd18
frandavo <frandavowork@gmail.com>
2025-03-27 13:48:08 +0100

    feat: prueba gifs con map 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, {useEffect,useState} from 'react';
 import './App.css';
-const GIFS = 
-[
-'https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExaG1jZTN4NTMyb2prNzZhYm5vNXUwbGFwaHY0ZW1xanFhbXFvc21obiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/WOGyW1zONpVHpMBbtl/giphy.gif',
-'https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExbnA2b25pODh6cWxqazlnem9wNnNvZjBrcjQzemxjbTdqeTAyazJ0biZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/gIwoiPd7e8bsck8RJ0/giphy.gif'
-]
+
+const apiURL='https://api.giphy.com/v1/gifs/search?api_key=lwM8kLL34CmoBAkK0hOrhnMbAyxuQQyJ&q=motos&limit=10&offset=0&rating=g&lang=en'
+
+
 function App() {
-  const [gifs, setGifs] =  useState(GIFS)
+  const [gifs, setGifs] =  useState([])
   
-  const DIFFERENTS_GIFS = ['https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExdWNyZ3cxOTB2dzd2cm5uZGJ4OWRhajdsbThyczV4N3Rlcnhid3BwdyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/dVtyXloxyUMMyFEG5Z/giphy.gif']
+  useEffect(function ()  {
+    fetch(apiURL)
+      .then(res => res.json())
+      .then(response => {
+        const{data=[]} = response
+        if(Array.isArray(data)){
+          const gifs = data.map(image => image.images.downsized_medium.url)
+          setGifs(gifs)
+        }
+      })
+  },[])
+
   return (
     <div className="App">
       <section className="App-content">
         {
-          gifs.map(singleGif => <img src={singleGif}/>)
+          gifs.map(singleGif => <img src={singleGif} alt="Gif" />)
         }
-        <button onClick={() => setGifs(DIFFERENTS_GIFS)}>Cambiar GIF</button>
       </section>
     </div>
   );
